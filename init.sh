c96f0e251f57d6e96109e31e808af50458f1a9c3
Thomas McQuitty <57770653+McQuitty-Broadcom@users.noreply.github.com>
2022-11-18 12:04:55 -0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -97,8 +97,8 @@ zowe plugins install @zowe/secure-credential-store-for-zowe-cli
 # code --install-extension broadcomMFD.data-editor-for-mainframe --force
 # code --install-extension broadcomMFD.bridge-for-git-explorer --force
 
-curl -sSL https://raw.githubusercontent.com/McQuitty-Broadcom/zowe-ubuntu/v2/zowe_configs/sr01/zowe.config.json > /root/zowe.config.json
-curl -sSL https://raw.githubusercontent.com/McQuitty-Broadcom/zowe-ubuntu/v2/zowe_configs/sr01/zowe.schema.json > /root/zowe.schema.json
+wget https://raw.githubusercontent.com/McQuitty-Broadcom/zowe-ubuntu/v2/zowe_configs/sr01/zowe.config.json 
+wget -sSL https://raw.githubusercontent.com/McQuitty-Broadcom/zowe-ubuntu/v2/zowe_configs/sr01/zowe.schema.json 
 
-cp ./zowe_configs/sr01/zowe.schema.json /root/.zowe/
+# cp ./zowe_configs/sr01/zowe.schema.json /root/.zowe/
 echo 'to use zowe, reboot and issue zowe config secure --gc'
